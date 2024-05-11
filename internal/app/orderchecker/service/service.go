6ece9ea816b108fd7b7843d7b72c5e360884be23
mustafadervisglu <mustafadervisoglu16@gmail.com>
2024-05-11 14:33:08 +0300

    feat(orderchecker/service): implement order matching logic with balance updates
    - Added `MatchOrder` function to match buy and sell orders based on price and update order quantities.
    - Introduced `UpdateUserBalances` to adjust user balances after successful order matches.
    - Implemented `SoftDeleteOrderMatch` to soft delete matched orders from the database.
    - Modified `ProcessTransactions` to integrate new matching and balance update logic, enhancing the order processing workflow.
    - Utilized `utils.MinQuantity` for calculating match quantities and `time.Now()` for setting order completion timestamps, improving order match accuracy and record-keeping.
    - Ensured error handling in database operations to maintain data integrity during order processing.
    
    refactor(transaction_repository.go): streamline transaction repo interface and implementations
    
    - Remove unused `MatchOrder` and `UpdateBalance` (old signature) methods to clean up the interface.
    - Introduce new methods for user and order management (`FindUserById`, `SoftDeleteOrder`, `UpdateOrders`, `FindOrderById`, `SoftDeleteMatch`, `FetchMatch`) to enhance functionality around order and user operations.
    - Replace `utils` package dependency with `errors` and `github.com/google/uuid` for error handling and UUID operations, respectively, to improve error management and ID handling.
    - Adjust `SaveMatches` and `UpdateBalance` (new signature) implementations to align with the updated interface requirements, ensuring more robust and flexible transaction processing.

--- internal/app/orderchecker/service/service.go
+++ internal/app/orderchecker/service/service.go
@@ -3,6 +3,8 @@ package service
 import (
 	"bitcoinOrder/internal/domain/entity"
 	"bitcoinOrder/internal/repository"
+	"bitcoinOrder/pkg/utils"
+	"time"
 )
 
 type OrderCheckerService struct {
@@ -15,22 +17,129 @@ func NewOrderCheckerService(repo repository.ITransactionRepository) *OrderChecke
 	}
 }
 
-type IOrderCheckerService interface {
-	ProcessTransactions() error
-	UpdateBalances(matches []entity.OrderMatch) error
+func (s *OrderCheckerService) MatchOrder(buyOrders, sellOrders []entity.Order) ([]entity.OrderMatch, error) {
+	var orderMatches []entity.OrderMatch
+	var ordersToUpdate []*entity.Order
+
+	for i := 0; i < len(buyOrders); i++ {
+		for j := 0; j < len(sellOrders); j++ {
+			buyOrder := &buyOrders[i]
+			sellOrder := &sellOrders[j]
+
+			if buyOrder.OrderPrice == sellOrder.OrderPrice {
+				matchQuantity := utils.MinQuantity(buyOrder.OrderQuantity, sellOrder.OrderQuantity)
+				orderMatch := entity.OrderMatch{
+					OrderID1:      buyOrder.ID,
+					OrderID2:      sellOrder.ID,
+					OrderQuantity: matchQuantity,
+					MatchedAt:     time.Now(),
+				}
+				orderMatches = append(orderMatches, orderMatch)
+
+				buyOrder.OrderQuantity -= matchQuantity
+				sellOrder.OrderQuantity -= matchQuantity
+
+				ordersToUpdate = append(ordersToUpdate, buyOrder, sellOrder)
+
+				now := time.Now()
+
+				if buyOrder.OrderQuantity == 0 {
+					buyOrder.OrderStatus = false
+					buyOrder.CompletedAt = &now
+
+				}
+				if sellOrder.OrderQuantity == 0 {
+					sellOrder.OrderStatus = false
+					sellOrder.CompletedAt = &now
+
+				}
+			}
+		}
+	}
+
+	if err := s.transactionRepo.UpdateOrders(ordersToUpdate); err != nil {
+		return nil, err
+	}
+
+	if err := s.transactionRepo.SaveMatches(orderMatches); err != nil {
+		return nil, err
+	}
+
+	return orderMatches, nil
+}
+
+func (s *OrderCheckerService) UpdateUserBalances(orderMatches []entity.OrderMatch) error {
+	for _, match := range orderMatches {
+		buyOrder, err := s.transactionRepo.FindOrderById(match.OrderID1)
+		if err != nil {
+			return err
+		}
+
+		sellOrder, err := s.transactionRepo.FindOrderById(match.OrderID2)
+		if err != nil {
+			return err
+		}
+
+		buyUser, err := s.transactionRepo.FindUserById(buyOrder.UserID)
+		if err != nil {
+			return err
+		}
+
+		sellUser, err := s.transactionRepo.FindUserById(sellOrder.UserID)
+		if err != nil {
+			return err
+		}
+
+		*buyUser.UsdBalance -= buyOrder.OrderPrice * match.OrderQuantity
+		*buyUser.BtcBalance += match.OrderQuantity
+		*sellUser.UsdBalance += sellOrder.OrderPrice * match.OrderQuantity
+		*sellUser.BtcBalance -= match.OrderQuantity
+
+		if err := s.transactionRepo.UpdateBalance([]*entity.Users{buyUser, sellUser}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *OrderCheckerService) SoftDeleteOrderMatch(orderMatches []entity.OrderMatch) error {
+	for _, match := range orderMatches {
+		fetchedMatch, err := s.transactionRepo.FetchMatch(match.OrderID1, match.OrderID2)
+		if err != nil {
+			return err
+		}
+		if fetchedMatch != nil {
+			if err := s.transactionRepo.SoftDeleteMatch(fetchedMatch.ID); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 func (s *OrderCheckerService) ProcessTransactions() error {
-	orderMatches, err := s.transactionRepo.CheckOrder()
+	buyOrders, err := s.transactionRepo.FindBuyOrders()
 	if err != nil {
 		return err
 	}
-	return s.UpdateBalances(orderMatches)
-}
-
-func (s *OrderCheckerService) UpdateBalances(matches []entity.OrderMatch) error {
-	if len(matches) == 0 {
-		return nil
+	sellOrders, err := s.transactionRepo.FindSellOrders()
+	if err != nil {
+		return err
 	}
-	return s.transactionRepo.UpdateBalance(matches)
+
+	orderMatches, err := s.MatchOrder(buyOrders, sellOrders)
+	if err != nil {
+		return err
+	}
+
+	if err := s.UpdateUserBalances(orderMatches); err != nil {
+		return err
+	}
+
+	if err := s.SoftDeleteOrderMatch(orderMatches); err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/repository/transaction_repository.go
+++ internal/repository/transaction_repository.go
@@ -2,17 +2,23 @@ package repository
 
 import (
 	"bitcoinOrder/internal/domain/entity"
-	"bitcoinOrder/pkg/utils"
+	"errors"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
 )
 
 type ITransactionRepository interface {
 	SaveMatches(orderMatches []entity.OrderMatch) error
-	UpdateBalance(orderMatches []entity.OrderMatch) error
-	MatchOrder(buyOrders, sellOrders []entity.Order) ([]entity.OrderMatch, error)
 	FindBuyOrders() ([]entity.Order, error)
 	FindSellOrders() ([]entity.Order, error)
+	UpdateBalance(users []*entity.Users) error
+	FindUserById(userId uuid.UUID) (*entity.Users, error)
+	SoftDeleteOrder(orderId uuid.UUID) error
+	UpdateOrders(orders []*entity.Order) error
+	FindOrderById(orderId uuid.UUID) (entity.Order, error)
+	SoftDeleteMatch(matchID uuid.UUID) error
+	FetchMatch(orderID1 uuid.UUID, orderID2 uuid.UUID) (*entity.OrderMatch, error)
 }
 
 type TransactionRepository struct {
@@ -24,67 +30,6 @@ func NewTransactionRepository(db *gorm.DB) ITransactionRepository {
 		gormDB: db,
 	}
 }
-
-//func (o *TransactionRepository) CheckOrder() ([]entity.OrderMatch, error) {
-//
-//	if o.gormDB == nil {
-//		return nil, errors.New("nil db connection")
-//	}
-//
-//	var orderMatches []entity.OrderMatch
-//	var buyOrders, sellOrders []entity.Order
-//	if err := o.gormDB.Where("order_status = ? AND type = ?", true, "buy").Order("order_price ASC, created_at ASC").Find(&buyOrders).Error; err != nil {
-//		return nil, err
-//	}
-//	if err := o.gormDB.Where("order_status = ? AND type = ?", true, "sell").Order("order_price DESC, created_at ASC").Find(&sellOrders).Error; err != nil {
-//		return nil, err
-//	}
-//
-//	for i := 0; i < len(buyOrders); i++ {
-//		for j := 0; j < len(sellOrders); j++ {
-//			buyOrder := buyOrders[i]
-//			sellOrder := sellOrders[j]
-//			if buyOrder.OrderPrice == sellOrder.OrderPrice {
-//				matchQuantity := utils.MinQuantity(buyOrder.OrderQuantity, sellOrder.OrderQuantity)
-//
-//				orderMatch := entity.OrderMatch{
-//					OrderID1:      buyOrder.ID,
-//					OrderID2:      sellOrder.ID,
-//					OrderQuantity: matchQuantity,
-//					MatchedAt:     time.Now(),
-//				}
-//
-//				if err := o.gormDB.Create(&orderMatch).Error; err != nil {
-//					return nil, err
-//				}
-//				orderMatches = append(orderMatches, orderMatch)
-//
-//				buyOrder.OrderQuantity -= matchQuantity
-//				sellOrder.OrderQuantity -= matchQuantity
-//
-//				if buyOrder.OrderQuantity == 0 {
-//					buyOrder.OrderStatus = false
-//					now := time.Now()
-//					buyOrder.CompletedAt = &now
-//				}
-//				if sellOrder.OrderQuantity == 0 {
-//					sellOrder.OrderStatus = false
-//					now := time.Now()
-//					sellOrder.CompletedAt = &now
-//				}
-//
-//				o.gormDB.Save(&buyOrder)
-//				o.gormDB.Save(&sellOrder)
-//
-//				if buyOrder.OrderQuantity == 0 || sellOrder.OrderQuantity == 0 {
-//					break
-//				}
-//			}
-//		}
-//	}
-//
-//	return orderMatches, nil
-//}
 
 func (o *TransactionRepository) FindSellOrders() ([]entity.Order, error) {
 	var sellOrders []entity.Order
@@ -108,62 +53,80 @@ func (o *TransactionRepository) FindBuyOrders() ([]entity.Order, error) {
 	return buyOrders, nil
 }
 
-func (o *TransactionRepository) MatchOrder(buyOrders, sellOrders []entity.Order) ([]entity.OrderMatch, error) {
-	var orderMatches []entity.OrderMatch
-	for i := 0; i < len(buyOrders); i++ {
-		for j := 0; j < len(sellOrders); j++ {
-			buyOrder := buyOrders[i]
-			sellOrder := sellOrders[j]
-			if sellOrder.OrderQuantity == buyOrder.OrderQuantity {
-				matchQuantity := utils.MinQuantity(buyOrder.OrderQuantity, sellOrder.OrderQuantity)
-				orderMatch := entity.OrderMatch{
-					OrderID1:      buyOrder.ID,
-					OrderID2:      sellOrder.ID,
-					OrderQuantity: matchQuantity,
-					MatchedAt:     time.Now(),
-				}
-				orderMatches = append(orderMatches, orderMatch)
-			}
-		}
-	}
-	return orderMatches, nil
-}
 func (o *TransactionRepository) SaveMatches(orderMatches []entity.OrderMatch) error {
+	if len(orderMatches) == 0 {
+		return nil
+	}
 	if err := o.gormDB.Create(&orderMatches).Error; err != nil {
 		return err
 	}
 	return nil
 }
-func (o *TransactionRepository) UpdateBalance(orderMatches []entity.OrderMatch) error {
-	for _, match := range orderMatches {
-		var buyOrder, sellOrder entity.Order
-		if err := o.gormDB.Take(&buyOrder, "id = ?", match.OrderID1).Error; err != nil {
-			return err
-		}
-		if err := o.gormDB.Take(&sellOrder, "id = ?", match.OrderID2).Error; err != nil {
-			return err
-		}
 
-		buyUser, err := utils.GetBalance(o.gormDB, buyOrder.UserID.String())
-		if err != nil {
-			return err
+func (o *TransactionRepository) UpdateBalance(users []*entity.Users) error {
+	for _, user := range users {
+		updates := map[string]interface{}{
+			"usd_balance": user.UsdBalance,
+			"btc_balance": user.BtcBalance,
 		}
-		sellUser, err := utils.GetBalance(o.gormDB, sellOrder.UserID.String())
-		if err != nil {
-			return err
-		}
-
-		*buyUser.UsdBalance -= buyOrder.OrderPrice * match.OrderQuantity
-		*buyUser.BtcBalance += match.OrderQuantity
-		*sellUser.UsdBalance += sellOrder.OrderPrice * match.OrderQuantity
-		*sellUser.BtcBalance -= match.OrderQuantity
-
-		if err := o.gormDB.Save(&buyUser).Error; err != nil {
-			return err
-		}
-		if err := o.gormDB.Save(&sellUser).Error; err != nil {
+		if err := o.gormDB.Model(user).Updates(updates).Error; err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func (o *TransactionRepository) FindOrderById(orderId uuid.UUID) (entity.Order, error) {
+	var order entity.Order
+	if err := o.gormDB.Take(&order, "id = ?", orderId).Error; err != nil {
+		return entity.Order{}, err
+	}
+	return order, nil
+}
+
+func (o *TransactionRepository) FindUserById(userId uuid.UUID) (*entity.Users, error) {
+	var user entity.Users
+	if err := o.gormDB.Take(&user, "id = ?", userId).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (o *TransactionRepository) SoftDeleteOrder(orderId uuid.UUID) error {
+	if err := o.gormDB.Model(&entity.Order{}).Where("id = ?", orderId).Update("deleted_at", time.Now()).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (o *TransactionRepository) UpdateOrders(orders []*entity.Order) error {
+	for _, order := range orders {
+		if err := o.gormDB.Model(order).Updates(map[string]interface{}{
+			"order_quantity": order.OrderQuantity,
+			"order_status":   order.OrderStatus,
+			"completed_at":   order.CompletedAt,
+		}).Error; err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (o *TransactionRepository) SoftDeleteMatch(matchID uuid.UUID) error {
+	if err := o.gormDB.Model(&entity.OrderMatch{}).Where("id = ?", matchID).Update("deleted_at", time.Now()).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (o *TransactionRepository) FetchMatch(orderID1 uuid.UUID, orderID2 uuid.UUID) (*entity.OrderMatch, error) {
+	var match entity.OrderMatch
+	err := o.gormDB.Model(&entity.OrderMatch{}).Where("order_id1 = ? AND order_id2 = ?", orderID1, orderID2).First(&match).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &match, nil
 }
